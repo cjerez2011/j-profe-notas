124b783153eb44ad29227cd81a9316fbbf382154
fcojavier.poncemartinez@gmail.com <fcojavier.poncemartinez@gmail.com@90b0cf7f-27c7-471e-ad92-b05c8a286746>
2014-10-10 21:50:53 +0000

    JavaBeans

--- trunk/webProfeNotas/src/java/modelo/Nota.java
+++ trunk/webProfeNotas/src/java/modelo/Nota.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package modelo;
 
 /**
@@ -11,5 +5,70 @@ package modelo;
  * @author Fco
  */
 public class Nota {
-    
+    private int id;
+    private float nota;
+    private int porcentaje;
+    private Alumno alumno;
+    private Curso curso;
+
+    public Nota(float nota, int porcentaje, Alumno alumno, Curso curso) {
+        this.nota = nota;
+        this.porcentaje = porcentaje;
+        this.alumno = alumno;
+        this.curso = curso;
+    }
+
+    public Nota(int id, float nota, int porcentaje, Alumno alumno, Curso curso) {
+        this.id = id;
+        this.nota = nota;
+        this.porcentaje = porcentaje;
+        this.alumno = alumno;
+        this.curso = curso;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getNota() {
+        return nota;
+    }
+
+    public void setNota(float nota) {
+        this.nota = nota;
+    }
+
+    public int getPorcentaje() {
+        return porcentaje;
+    }
+
+    public void setPorcentaje(int porcentaje) {
+        this.porcentaje = porcentaje;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    @Override
+    public String toString() {
+        return  nota + "" + porcentaje + "" + alumno + "" + curso;
+    }
+
 }
