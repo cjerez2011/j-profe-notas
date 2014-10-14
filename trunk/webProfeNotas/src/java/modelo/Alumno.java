8a842a0c4150596476eaa11ef9b69156ebe1b1f4
borinho.bj@gmail.com <borinho.bj@gmail.com@90b0cf7f-27c7-471e-ad92-b05c8a286746>
2014-10-14 18:53:35 +0000

    se agregan atributos a clases alumnos, Curso, se crean constructores, se crean metodos en DAO, se crea servlet para que otro cree ventana de ingreso de notas...

--- trunk/webProfeNotas/src/java/modelo/Alumno.java
+++ trunk/webProfeNotas/src/java/modelo/Alumno.java
@@ -1,15 +1,117 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package modelo;
 
-/**
- *
- * @author Fco
- */
+
 public class Alumno {
+    
+  private  int id, edad;
+  private   String rut,nombre,ape_pat,ape_mat,sexo;
+  private Nota nota;
+  private Curso curso;
+  
+  
+  
+    public Alumno(String rut, String nombre, String ape_pat, String ape_mat, Nota nota, Curso curso) {
+        this.rut = rut;
+        this.nombre = nombre;
+        this.ape_pat = ape_pat;
+        this.ape_mat = ape_mat;
+        this.nota = nota;
+        this.curso = curso;
+    }
+
+    public Alumno( String rut, String nombre, String ape_pat, String ape_mat, String sexo,int edad) {
+        this.edad = edad;
+        this.rut = rut;
+        this.nombre = nombre;
+        this.ape_pat = ape_pat;
+        this.ape_mat = ape_mat;
+        this.sexo = sexo;
+    }
+
+    
+    
+    
+
+    public Nota getNota() {
+        return nota;
+    }
+
+    public void setNota(Nota nota) {
+        this.nota = nota;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+  
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+  
+  
+  
+  
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApe_pat() {
+        return ape_pat;
+    }
+
+    public void setApe_pat(String ape_pat) {
+        this.ape_pat = ape_pat;
+    }
+
+    public String getApe_mat() {
+        return ape_mat;
+    }
+
+    public void setApe_mat(String ape_mat) {
+        this.ape_mat = ape_mat;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+    
+    
     
 }
